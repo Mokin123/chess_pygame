d7e13313d1badecb054e6d7d95c7c07bb5175f08
Eric Mok <ericmok@Erics-MacBook-Air-2.local>
2022-12-12 22:06:44 +0800

    - Able to create the basic board for the chess  - Able to create the individual pieces and where they are supposed to go through the use of a 2D array  - Able to create a function which moves the pieces to a desired location  - Able to make the pieces able to capture other pieces (though this also work for capturing your own pieces) > > What I need to work on: > - Making a rule which only allows the correct colored pieces to moves at the correct time  - Making rules for the individual chess pieces  - Applying other chess rule like  castling or En passant  - Maybe make a 1 vs bot function  - Create a starting page  - Create a user friendly UI which shows who's turn it is and pieces captured etc...  - Making a drag function to be able to drag the individual pieces instead of clicking to select a piece

---
+++ images.py
@@ -0,0 +1,22 @@
+import pygame
+
+chess_board = pygame.image.load(f'assets/chess_board.png')
+
+
+
+black_pawn = pygame.image.load(f'/Users/ericmok/Desktop/Python_stuff/chess_pygame/assets/black/black_pawn.png')
+black_bishop = pygame.image.load(f'/Users/ericmok/Desktop/Python_stuff/chess_pygame/assets/black/black_bishop.png')
+black_knight = pygame.image.load(f'/Users/ericmok/Desktop/Python_stuff/chess_pygame/assets/black/black_knight.png')
+black_rook = pygame.image.load(f'/Users/ericmok/Desktop/Python_stuff/chess_pygame/assets/black/black_rook.png')
+black_queen = pygame.image.load(f'/Users/ericmok/Desktop/Python_stuff/chess_pygame/assets/black/black_queen.png')
+black_king = pygame.image.load(f'/Users/ericmok/Desktop/Python_stuff/chess_pygame/assets/black/black_king.png')
+
+
+
+
+white_pawn = pygame.image.load(f'/Users/ericmok/Desktop/Python_stuff/chess_pygame/assets/white/white_pawn.png')
+white_bishop = pygame.image.load(f'/Users/ericmok/Desktop/Python_stuff/chess_pygame/assets/white/white_bishop.png')
+white_knight = pygame.image.load(f'/Users/ericmok/Desktop/Python_stuff/chess_pygame/assets/white/white_knight.png')
+white_rook = pygame.image.load(f'/Users/ericmok/Desktop/Python_stuff/chess_pygame/assets/white/white_rook.png')
+white_queen = pygame.image.load(f'/Users/ericmok/Desktop/Python_stuff/chess_pygame/assets/white/white_queen.png')
+white_king = pygame.image.load(f'/Users/ericmok/Desktop/Python_stuff/chess_pygame/assets/white/white_king.png')
---
+++ main.py
@@ -0,0 +1,136 @@
+import pygame
+import math
+from board_loc import*
+from images import *
+from string import ascii_uppercase
+
+pygame.init()
+screen_width, screen_height = 800, 800
+scaling_factor = 1
+clicked = False
+click1 = False
+# with reference to the board
+click1_pos = 0
+
+click2 = False
+# with reference to the board
+click2_pos = 0
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+board = [
+    [14,12,13,15,16,13,12,14],
+    [11,11,11,11,11,11,11,11],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [21,21,21,21,21,21,21,21],
+    [24,22,23,25,26,23,22,24]
+
+]
+
+
+
+
+def draw_board ():
+    screen.blit(chess_board,(0,0))
+
+
+def draw_piece():
+    for i in range (8):
+        for x in range (8):
+            a = (x)*100
+            b = (i)*100
+            # for black pieces
+            if board [i][x] < 20 and board [i][x] > 0:
+                if board [i][x] == 11:
+                    screen.blit(black_pawn,(a,b))
+                elif board [i][x] == 12:
+                    screen.blit(black_knight,(a,b))
+                elif board [i][x] == 13:
+                    screen.blit(black_bishop,(a,b))
+                elif board [i][x] == 14:
+                    screen.blit(black_rook,(a,b))
+                elif board [i][x] == 15:
+                    screen.blit(black_queen,(a,b))
+                elif board [i][x] == 16:
+                    screen.blit(black_king,(a,b))
+            # white pieces
+            elif board [i][x] > 20:
+                if board [i][x] == 21:
+                    screen.blit(white_pawn,(a,b))
+                elif board [i][x] == 22:
+                    screen.blit(white_knight,(a,b))
+                elif board [i][x] == 23:
+                    screen.blit(white_bishop,(a,b))
+                elif board [i][x] == 24:
+                    screen.blit(white_rook,(a,b))
+                elif board [i][x] == 25:
+                    screen.blit(white_queen,(a,b))
+                elif board [i][x] == 26:
+                    screen.blit(white_king,(a,b))
+
+# check if there is a piece in the mouse_position
+def check_piece(x,y):
+    first = int(y/100)
+    second = int(x/100)
+    return board[first][second]
+
+def get_pos(x,y):
+    first = int(y/100)
+    second = int(x/100)
+    return first,second
+
+# moving a piece to the wanted location
+def move_piece(old,new):
+    if board[new[0]][new[1]] != board[old[0]][old[1]]:
+        board[new[0]][new[1]] = board[old[0]][old[1]]
+        board[old[0]][old[1]] = 0
+#           current position of the piece
+# def pawn_rule(current,piece):
+#     # for white
+#     if piece >20:
+#         if current
+#
+#     # for black
+#     if piece < 20
+#         print(dsf)
+
+
+
+
+
+
+
+run = True
+while run:
+    draw_board()
+    draw_piece()
+    mouse_pos = pygame.mouse.get_pos()
+    clicks = pygame.mouse.get_pressed()
+
+    # event is stuff happening to the mouse
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            check = check_piece(mouse_pos[0],mouse_pos[1])
+            if check != 0 and click1 == False:
+                click1_pos = get_pos(mouse_pos[0],mouse_pos[1])
+                click1 = True
+            elif click1 == True:
+                click2_pos = get_pos(mouse_pos[0],mouse_pos[1])
+                click2 = True
+            clicked = True
+        if event.type == pygame.MOUSEBUTTONUP and event.button == 1 and clicked:
+            clicked = False
+            if click1 == True and click2 == True:
+                move_piece(click1_pos,click2_pos)
+                click1 = False
+                click2 = False
+                
+    # pygame.draw.rect(screen, white, (x, y, rect_width, rect_height))
+    pygame.display.update()
+
+pygame.quit()
